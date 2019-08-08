752dc73d1f7fda5fada45886ce09679d03ef1159
Abby Thompson <abby.thompson@myob.com>
2019-08-08 13:03:34 +1200

    restrictions almost finished

--- LINQ.Exercises/LINQ.Exercises/Restriction.cs
+++ LINQ.Exercises/LINQ.Exercises/Restriction.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -25,6 +26,7 @@ namespace LINQ.Exercises
         public void Where_n_is_less_than_or_equal_to_0_returns_expected_ints()
         {
             IEnumerable<int> result = TestData.Numbers;
+            result = result.Where(n => n <= 0);
 
             Assert.Equal(5, result.Count());
             Assert.True(new[] { -3, -1, -4, -1, -5 }.SequenceEqual(result));
@@ -34,6 +36,7 @@ namespace LINQ.Exercises
         public void Where_n_multiplied_by_2_is_greater_than_5()
         {
             IEnumerable<int> result = TestData.Numbers;
+            result = result.Where(n => n * 2 > 5);
 
             Assert.True(new[] { 3, 5 }.SequenceEqual(result));
         }
@@ -42,6 +45,9 @@ namespace LINQ.Exercises
         public void Where_n_is_even()
         {
             IEnumerable<int> result = TestData.Numbers;
+            
+            result = result.Where(n => n % 2 == 0);
+
 
             Assert.True(new[] { 2, -4 }.SequenceEqual(result));
         }
@@ -50,6 +56,8 @@ namespace LINQ.Exercises
         public void Where_index_of_n_is_odd()
         {
             IEnumerable<int> result = TestData.Numbers;
+            
+            result = result.Where(n => n % 2 > 0);
 
             Assert.True(new[] { 1, 1, 3, 5 }.SequenceEqual(result));
         }
@@ -58,6 +66,9 @@ namespace LINQ.Exercises
         public void Where_n_is_even_and_n_is_less_than_0()
         {
             IEnumerable<int> result = TestData.Numbers;
+            
+            result = result.Where(n => n % 2 == 0 && n < 0);
+
 
             Assert.True(new[] { -4 }.SequenceEqual(result));
         }
@@ -67,6 +78,9 @@ namespace LINQ.Exercises
         {
             // n * n - 2 * n
             IEnumerable<int> result = TestData.Numbers;
+            
+            result = result.Where(n => (n * n - 2 * n) > n);
+
 
             Assert.True(new[] { -3, -1, -4, -1, 5, -5 }.SequenceEqual(result));
         }
@@ -76,6 +90,9 @@ namespace LINQ.Exercises
         {
             IEnumerable<string> result = TestData.Animals;
 
+            result = result.Where(n => n.Length < 5).Take(1);
+
+
             Assert.Equal(1, result.Count());
         }
 
@@ -83,6 +100,8 @@ namespace LINQ.Exercises
         public void Where_string_length_is_9_returns_expected_strings()
         {
             IEnumerable<string> result = TestData.Animals;
+
+            result = result.Where(n => n.Length == 9);
 
             Assert.Equal(1, result.Count());
             Assert.True(new[] { "swordfish" }.SequenceEqual(result));
@@ -93,6 +112,8 @@ namespace LINQ.Exercises
         {
             IEnumerable<string> result = TestData.Animals;
 
+            result = result.Where(s => s.StartsWith("s"));
+
             Assert.True(new[] { "swordfish", "shark" }.SequenceEqual(result));
         }
 
@@ -100,6 +121,8 @@ namespace LINQ.Exercises
         public void Where_string_has_i_as_a_second_letter()
         {
             IEnumerable<string> result = TestData.Animals;
+
+            result = result.Where(s => s.IndexOf("i", StringComparison.Ordinal) == 1);
 
             Assert.True(new[] { "tiger", "lion" }.SequenceEqual(result));
         }
@@ -109,6 +132,8 @@ namespace LINQ.Exercises
         {
             IEnumerable<string> result = TestData.Animals;
 
+            result = result.Where(n => n.Contains("e"));
+
             Assert.True(new[] { "tiger", "penguin", "elephant" }.SequenceEqual(result));
         }
 
@@ -116,6 +141,8 @@ namespace LINQ.Exercises
         public void Where_string_ends_with_uppercase_t()
         {
             IEnumerable<string> result = TestData.Animals;
+
+            result = result.Where(s => s.EndsWith("t"));
 
             Assert.True(new[] { "elephant" }.SequenceEqual(result));
         }
@@ -125,6 +152,8 @@ namespace LINQ.Exercises
         {
             IEnumerable<string> result = TestData.Animals;
 
+            result = result.Where(s => s.Contains("io"));
+
             Assert.True(new[] { "lion" }.SequenceEqual(result));
         }
 
@@ -132,6 +161,8 @@ namespace LINQ.Exercises
         public void Where_person_firstname_and_lastname_starts_with_same_letter()
         {
             IEnumerable<TestData.Person> result = TestData.People;
+
+            result = result.Where(person => person.FirstName[0] == person.LastName[0]);
 
             Assert.True(new[] { TestData.People[3] }.SequenceEqual(result));
         }
@@ -141,6 +172,8 @@ namespace LINQ.Exercises
         {
             IEnumerable<TestData.Person> result = TestData.People;
 
+            result = result.Where(person => person.Born.Year < 1990);
+
             Assert.True(new[] { TestData.People[1], TestData.People[3] }.SequenceEqual(result));
         }
 
@@ -148,6 +181,8 @@ namespace LINQ.Exercises
         public void Where_person_was_born_on_day_with_even_number()
         {
             IEnumerable<TestData.Person> result = TestData.People;
+
+            result = result.Where(person => person.Born.Day % 2 == 0);
 
             Assert.True(new[] { TestData.People[0], TestData.People[3] }.SequenceEqual(result));
         }
@@ -157,6 +192,8 @@ namespace LINQ.Exercises
         {
             IEnumerable<TestData.Person> result = TestData.People;
 
+            result = result.Where(person => person.Born.Day == 21 && person.Born.DayOfWeek == DayOfWeek.Monday);
+
             Assert.True(new[] { TestData.People[2] }.SequenceEqual(result));
         }
 
@@ -165,6 +202,8 @@ namespace LINQ.Exercises
         {
             IEnumerable<TestData.Person> result = TestData.People;
 
+            result = result.Where(person => person.Born.Year == (2000 - 19));
+
             Assert.True(new[] { TestData.People[1], TestData.People[3] }.SequenceEqual(result));
         }
 
@@ -172,6 +211,8 @@ namespace LINQ.Exercises
         public void Where_person_lastname_contains_ll_and_sum_of_year_month_day_is_greater_than_2000()
         {
             IEnumerable<TestData.Person> result = TestData.People;
+
+            result = result.Where(person => person.LastName.Contains("ll") && person.Born.Day + person.Born.Month + person.Born.Year > 2000);
 
             Assert.True(new[] { TestData.People[2] }.SequenceEqual(result));
         }
